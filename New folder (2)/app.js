e3f64ab05fd07995b6febae5913ce00df57a4bd5
Yeasin Arafat <75704262+ya-shuvo30@users.noreply.github.com>
2022-01-13 00:12:24 +0600

    Add files via upload

---
+++ New folder (2)/app.js
@@ -0,0 +1,7 @@
+var height = 5.8;
+var Weight = 55 ;
+var BMI;
+
+height = height / 3.281;
+BMI = Weight /(height*height);
+console.log(BMI);
---
+++ New folder/app.js
@@ -0,0 +1,6 @@
+ var num = prompt("Enter the value :");
+
+ console.log(num ,   (typeof(num)));
+ num = Number(num);
+
+ console.log(num ,   (typeof(num)));
